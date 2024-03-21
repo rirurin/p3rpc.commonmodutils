278e5624dd5bc0883be8b7c0f3a3b83aff8027e6
Rirurin <rirurin@rirurin.com>
2024-03-21 20:00:53 +0800

    notify on new object, spawn object

--- p3rpc.commonmodutils/Context.cs
+++ p3rpc.commonmodutils/Context.cs
@@ -6,6 +6,7 @@ using Reloaded.Hooks.Definitions;
 using SharedScans.Interfaces;
 using System.Data;
 using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
 
 #pragma warning disable CS1591
 
@@ -13,7 +14,6 @@ namespace p3rpc.commonmodutils
 {
     public abstract class Context
     {
-        public static int elementsPerChunk = 65536;
         public long _baseAddress { get; init; }
         public IConfigurable _config { get; set; }
         public ILogger _logger { get; init; }
@@ -44,6 +44,8 @@ namespace p3rpc.commonmodutils
     // For Unreal Engine games (Persona 3 Reload, future UE Atlus games)
     public class UnrealContext : Context
     {
+        // "We have UE4SS at home"
+        // UE4SS at home:
         public abstract class FindObjectBase
         {
             protected UnrealContext Context { get; init; }
@@ -105,6 +107,11 @@ namespace p3rpc.commonmodutils
         private Thread _findObjectThread { get; init; }
 
         private BlockingCollection<FindObjectBase> _findObjects = new();
+
+        private IHook<ICommonMethods.StaticConstructObject_Internal> _staticConstructObject;
+        private Dictionary<string, List<Action<nint>>> _objectListeners = new();
+        private IHook<ICommonMethods.GetPrivateStaticClassBody> _staticClassBody;
+        private Dictionary<string, nint> _classNameToType = new();
         protected nuint TransformAddressForFUObjectArray(int offset) => Utils.GetGlobalAddress((nint)(_baseAddress + offset + 3)) - 0x10;
         public UnrealContext(long baseAddress, IConfigurable config, ILogger logger, IStartupScanner startupScanner,
             IReloadedHooks hooks, string modLocation, Utils utils, Memory memory, ISharedScans sharedScans)
@@ -114,15 +121,45 @@ namespace p3rpc.commonmodutils
             {
                 _sharedScans.CreateListener("FUObjectArray", addr => _utils.AfterSigScan(addr, TransformAddressForFUObjectArray, addr => g_objectArray = (FUObjectArray*)addr));
                 _sharedScans.CreateListener("FGlobalNamePool", addr => _utils.AfterSigScan(addr, _utils.GetIndirectAddressLong, addr => g_namePool = (FNamePool*)addr));
+                _sharedScans.CreateListener("StaticConstructObject_Internal", addr => _utils.AfterSigScan(addr, _utils.GetDirectAddress, addr => _staticConstructObject = _utils.MakeHooker<ICommonMethods.StaticConstructObject_Internal>(StaticConstructObject_InternalImpl, addr)));
+                _sharedScans.CreateListener("GetPrivateStaticClassBody", addr => _utils.AfterSigScan(addr, _utils.GetDirectAddress, addr => _staticClassBody = _utils.MakeHooker<ICommonMethods.GetPrivateStaticClassBody>(GetPrivateStaticClassBodyImpl, addr)));
             }
             _findObjectThread = new Thread(new ThreadStart(ProcessObjectQueue));
             _findObjectThread.IsBackground = true;
             _findObjectThread.Start();
         }
-        // "We have UE4SS at home"
-        // UE4SS at home:
+        public unsafe string GetFName(FName name) => g_namePool->GetString(name);
         public unsafe string GetObjectName(UObject* obj) => g_namePool->GetString(obj->NamePrivate);
+
+        private unsafe string GetPathName(UObject* obj, UObject* end)
+        {
+            var path = GetObjectName(obj);
+            if (obj->OuterPrivate != null)
+            {
+                var separator = obj == end ? ":" : ".";
+                path = $"{GetPathName(obj->OuterPrivate, end)}{separator}{path}";
+            }
+            return path;
+        }
+        public unsafe string GetFullName(UObject* obj) // path name used throughout UE4SS
+        {
+            return obj->OuterPrivate != null ? GetPathName(obj, obj) : GetObjectName(obj);
+        }
         public unsafe string GetObjectType(UObject* obj) => g_namePool->GetString(((UObject*)obj->ClassPrivate)->NamePrivate);
+        public unsafe UClass* GetType(string type) => (UClass*)FindObject(type, "Class");
+        public unsafe void GetTypeAsync(string type, Action<nint> foundCb) => FindObjectAsync(type, "Class", foundCb);
+        public unsafe bool IsObjectSubclassOf(UObject* obj, UClass* type)
+        {
+            var currType = obj->ClassPrivate;
+            while (currType != null)
+            {
+                if (g_namePool->GetString(((UObject*)currType)->NamePrivate).Equals("Object")) break; // UObject is base type
+                if (((UObject*)currType)->NamePrivate.Equals(((UObject*)type)->NamePrivate))
+                    return true;
+                currType = (UClass*)currType->_super.super_struct;
+            }
+            return false;
+        }
         private unsafe bool DoesNameMatch(UObject* tgtObj, string name) => g_namePool->GetString(tgtObj->NamePrivate).Equals(name);
         private unsafe bool DoesClassMatch(UObject* tgtObj, string name) => g_namePool->GetString(((UObject*)tgtObj->ClassPrivate)->NamePrivate).Equals(name);
         private unsafe void ForEachObject(Action<nint> objItem)
@@ -150,20 +187,20 @@ namespace p3rpc.commonmodutils
                 }
             });
             return ret;
-            /*
-            for (int i = 0; i < g_objectArray->NumElements; i++)
+        }
+        public unsafe ICollection<nint> FindAllObjectsNamed(string targetObj, string? objType = null)
+        {
+            var objects = new List<nint>();
+            ForEachObject(currAddr =>
             {
-                var currObj = &g_objectArray->Objects[i >> 0x10][i & 0xffff];
-                if (currObj->Object == null || (currObj->Flags & EInternalObjectFlags.Unreachable) != 0) continue;
+                var currObj = (FUObjectItem*)currAddr;
                 if (DoesNameMatch(currObj->Object, targetObj))
                 {
                     if (objType == null || DoesClassMatch(currObj->Object, objType))
-                        return currObj->Object;
+                        objects.Add((nint)currObj->Object);
                 }
-                //_logger.WriteLineAsync($"FindObject: ({g_namePool->GetString(((UObject*)currObj->Object->ClassPrivate)->NamePrivate)}) {g_namePool->GetString(currObj->Object->NamePrivate)}");
-            }
-            return null;
-            */
+            });
+            return objects;
         }
         public unsafe UObject* FindFirstOf(string objType)
         {
@@ -190,6 +227,47 @@ namespace p3rpc.commonmodutils
             });
             return objects;
         }
+        // Convenience functions
+        public unsafe UObject* GetEngineTransient() => FindObject("/Engine/Transient", "Package");
+        // void cb -> Action<UObject*>
+        public unsafe void NotifyOnNewObject(UClass* type, Action<nint> cb) => NotifyOnNewObject(GetObjectName((UObject*)type), cb);
+        public unsafe void NotifyOnNewObject(string typeName, Action<nint> cb)
+        {
+            if (_objectListeners.TryGetValue(typeName, out var listener)) listener.Add(cb);
+            else _objectListeners.Add(typeName, new() { cb });
+        }
+        public unsafe UObject* SpawnObject(UClass* type, UObject* owner)
+        {
+            var objParams = (FStaticConstructObjectParameters*)NativeMemory.AllocZeroed((nuint)sizeof(FStaticConstructObjectParameters));
+            objParams->Class = type;
+            objParams->Outer = owner;
+            //objParams->Name = name;
+            var newObj = StaticConstructObject_InternalImpl(objParams);
+            NativeMemory.Free(objParams);
+            return newObj;
+        }
+        public unsafe UObject* SpawnObject(string type, UObject* owner)
+        {
+            if (_classNameToType.TryGetValue(type, out var typePtr))
+            {
+                var objParams = (FStaticConstructObjectParameters*)NativeMemory.AllocZeroed((nuint)sizeof(FStaticConstructObjectParameters));
+                objParams->Class = (UClass*)typePtr;
+                objParams->Outer = owner;
+                //objParams->Name = name;
+                var newObj = StaticConstructObject_InternalImpl(objParams);
+                NativeMemory.Free(objParams);
+                return newObj;
+            }
+            return null;
+        }
+        public unsafe void FreeObject(UObject* obj)
+        {
+            // TODO: Invoke GMalloc::Free
+        }
+        public unsafe void FreeObject(string objName, string typeName)
+        {
+            // TODO: Invoke GMalloc::Free
+        }
         public unsafe void FindObjectAsync(string targetObj, string? objType, Action<nint> foundCb) => _findObjects.Add(new FindObjectByName(this, targetObj, objType, foundCb));
         public unsafe void FindObjectAsync(string targetObj, Action<nint> foundCb) => FindObjectAsync(targetObj, null, foundCb);
         public unsafe void FindFirstOfAsync(string objType, Action<nint> foundCb) => _findObjects.Add(new FindObjectFirstOfType(this, objType, foundCb));
@@ -204,6 +282,39 @@ namespace p3rpc.commonmodutils
                         currFindObj.Execute();
                 }
             } catch (OperationCanceledException) { } // Called during process termination
+        }
+
+        private unsafe UObject* StaticConstructObject_InternalImpl(FStaticConstructObjectParameters* pParams)
+        {
+            var newObj = _staticConstructObject.OriginalFunction(pParams);
+            if (_objectListeners.TryGetValue(GetObjectType(newObj), out var listeners))
+                foreach (var listener in listeners) listener((nint)newObj);
+            return newObj;
+        }
+
+        private unsafe void GetPrivateStaticClassBodyImpl(
+            nint packageName,
+            nint name,
+            UClass** returnClass,
+            nint registerNativeFunc,
+            uint size,
+            uint align,
+            uint flags,
+            ulong castFlags,
+            nint config,
+            nint inClassCtor,
+            nint vtableHelperCtorCaller,
+            nint addRefObjects,
+            nint superFn,
+            nint withinFn,
+            byte isDynamic,
+            nint dynamicFn)
+        {
+            _staticClassBody.OriginalFunction(packageName, name, returnClass, registerNativeFunc, size, align, flags, castFlags, 
+                config, inClassCtor, vtableHelperCtorCaller, addRefObjects, superFn, withinFn, isDynamic, dynamicFn);
+            var className = Marshal.PtrToStringUni(name);
+            if (className != null)
+                _classNameToType.Add(className, *(nint*)returnClass);
         }
     }
 }
